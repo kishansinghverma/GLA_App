46039c8cb349bba5f53d4e8bd5eb7c5fc196faf4
kishansinghverma <kishansinghverma@gmail.com>
2019-03-28 22:49:09 +0530

    ui Fix

--- app/src/main/java/com/example/gla_app/Disp_Que.java
+++ app/src/main/java/com/example/gla_app/Disp_Que.java
@@ -6,14 +6,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class Disp_Que extends AppCompatActivity{
 
     RadioButton rb[]= new RadioButton[4];
+    RadioGroup rg;
     Button next;
     TextView qv;
     ArrayList<Question> questions;
@@ -21,17 +22,22 @@ public class Disp_Que extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.disp_que);
+        setContentView(R.layout.activity_disp_que);
 
-        Intent it=getIntent();
-        questions=(ArrayList<Question>) it.getSerializableExtra("list");
+        Intent intent=getIntent();
+        questions=(ArrayList<Question>) intent.getSerializableExtra("list");
         int id[]={R.id.radioButton1, R.id.radioButton2, R.id.radioButton3, R.id.radioButton4,};
 
-        for(int i=0; i<rb.length; i++)
-            rb[i]=findViewById(id[i]);
+        for(int i=0; i<rb.length; i++) {
+            rb[i] = findViewById(id[i]);
+            rb[i].setId(i+1);
+        }
+
 
         next=findViewById(R.id.button2);
         qv=findViewById(R.id.textView0);
+        rg=findViewById(R.id.radiogroup);
+
 
         //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -44,17 +50,21 @@ public class Disp_Que extends AppCompatActivity{
             @Override
             public void onClick(View v) {
 
+                questions.get(counter).setSelected(rg.getCheckedRadioButtonId());
                 if(counter<questions.size()-1) {
                     counter++;
+                    rg.clearCheck();
                     qv.setText("Q" + String.valueOf(counter + 1) + ": " + questions.get(counter).getQue());
+
                     for (int i = 0; i < rb.length; i++)
                         rb[i].setText(questions.get(counter).getOptions()[i]);
 
                     if(counter==questions.size()-1)
                         next.setText("Submit");
+
                 }
                 else
-                    startActivity(new Intent(Disp_Que.this, Result.class));
+                    startActivity(new Intent(Disp_Que.this, Result.class).putExtra("list", questions));
             }
         });
 
--- app/src/main/java/com/example/gla_app/Result.java
+++ app/src/main/java/com/example/gla_app/Result.java
@@ -1,7 +1,11 @@
 package com.example.gla_app;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class Result extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class Result extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        Intent intent=getIntent();
+
+        ArrayList<Question> questions= (ArrayList<Question>) intent.getSerializableExtra("list");
+
+        StringBuffer str=new StringBuffer();
+
+        for(Question x:questions)
+            str.append(String.valueOf(x.getSelected())+" ");
+
+        ((TextView)findViewById(R.id.textView2)).setText(str);
+
     }
 }
