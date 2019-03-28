877f05813b54aec66ccd89b793c85e1e015f4d3a
kishansinghverma <kishansinghverma@gmail.com>
2019-03-28 20:35:24 +0530

    init commit

--- app/src/main/java/com/example/gla_app/Disp_Que.java
+++ app/src/main/java/com/example/gla_app/Disp_Que.java
@@ -21,11 +21,11 @@ public class Disp_Que extends AppCompatActivity{
 
         ArrayList<Question> questions=(ArrayList<Question>) it.getSerializableExtra("list");
 
-        ((TextView)findViewById(R.id.textView2)).setText(questions.get(0).getQue());
-        ((TextView)findViewById(R.id.radioButton2)).setText(questions.get(0).getOptions()[0]);
-        ((TextView)findViewById(R.id.radioButton2)).setText(questions.get(0).getOptions()[1]);
-        ((TextView)findViewById(R.id.radioButton2)).setText(questions.get(0).getOptions()[2]);
-        ((TextView)findViewById(R.id.radioButton2)).setText(questions.get(0).getOptions()[3]);
+        ((TextView)findViewById(R.id.textView0)).setText(questions.get(0).getQue());
+        ((TextView)findViewById(R.id.textView1)).setText(questions.get(0).getOptions()[0]);
+        ((TextView)findViewById(R.id.textView2)).setText(questions.get(0).getOptions()[1]);
+        ((TextView)findViewById(R.id.textView3)).setText(questions.get(0).getOptions()[2]);
+        ((TextView)findViewById(R.id.textView4)).setText(questions.get(0).getOptions()[3]);
 
 
     }
